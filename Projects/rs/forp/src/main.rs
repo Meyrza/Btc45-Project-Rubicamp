cbff390c822258a1b27064d1ed221e2cff29a876
Meyrza <karkhi921@gmail.com>
2026-01-14 11:07:36 +0700

    up to repo new for2

--- Projects/rs/forp/src/main.rs
+++ Projects/rs/forp/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    for1()
+    //for1()
 }
 
-fn for1() {
-    for i in 0..=5 {
+// fn for1() {
+//     for i in 0..=5 {
+//         println!("{}", i)
+//     }
+// }
+
+fn for2() {
+    for i in (2..=20).step_by(2){
         println!("{}", i)
     }
 }
